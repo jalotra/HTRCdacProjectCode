9c2efa587708887d12578ddfaac63a00f5d58a0a
Shivam <jalotrashivam9@gmail.com>
2020-05-24 13:27:29 +0530

    Writing to files done

--- batch_jobs_htr.py
+++ batch_jobs_htr.py
@@ -29,7 +29,8 @@ def parallel_recognise():
         results = executor.map(validate.main, folders)
     print(*results)
     # Lets write the results to appropriate folders --> Output_Text/folder_name
-    for i in range(*results):
+    results = list(results)
+    for i in range(len(results)):
         with open(f"Output_Text/{results[0]}", "a") as f:
             f.write(results[1] + "\n")
 
